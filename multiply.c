2cfe0cbb81e5ce892481595ce3a15d744e8c6d53
blazeprix <75963104+blazeprix@users.noreply.github.com>
2020-12-28 11:06:03 +0700

    Update multiply.c

--- multiply.c
+++ multiply.c
@@ -3,7 +3,8 @@ int main(void){
 	int i, num;
 		printf("Input : ", num);
 		scanf("%d",&num);
-		for(i = 1; i < 13; i++)
+		for(i = 1; i < 13; i++){
 			printf("%d * %d = %d\n",num,i,i*num);
+		}
 	return 0;
 }
